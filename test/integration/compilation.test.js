61e0a1db0cc66fb7b8dce60027132550b142b97f
Kunal Nagpal <kunagpal@gmail.com>
2018-09-20 13:51:14 +0530

    Updated integration tests to account for a multitude of draft versions

--- test/integration/compilation.test.js
+++ test/integration/compilation.test.js
@@ -1,6 +1,6 @@
 /* global describe, it, expect */
 
-var fs = require('fs'),
+const fs = require('fs'),
     path = require('path'),
 
     Ajv = require('ajv'),
@@ -11,26 +11,30 @@ var fs = require('fs'),
     META_SCHEMA = require('ajv/lib/refs/json-schema-draft-04.json');
 
 describe('compilation', function () {
-    fs.readdirSync(schemaDir).forEach(function (version) {
-        it(version + ' format matches the draft schema', function (done) {
-            const schemaDirV1 = path.join(schemaDir, version),
-                schemaPath = path.join(schemaDirV1, 'collection.json'),
-                generatedSchema = schema.compile(schemaPath, schemaDirV1), // The JSON to check.
-                validator = new Ajv({
-                    schemaId: 'id', // only use id keyword as schema URI
-                    meta: false, // to prevent adding draft-06 meta-schema
-                    allErrors: true // check all rules collecting all errors
+    fs.readdirSync(schemaDir).forEach((draft) => {
+        describe(draft, function () {
+            fs.readdirSync(path.join(schemaDir, draft)).forEach((version) => {
+                it(`${version} format matches the draft ${draft} schema`, function (done) {
+                    const schemaDirV1 = path.join(schemaDir, draft, version),
+                        schemaPath = path.join(schemaDirV1, 'collection.json'),
+                        generatedSchema = schema.compile(schemaPath, schemaDirV1), // The JSON to check.
+                        validator = new Ajv({
+                            schemaId: 'id', // only use id keyword as schema URI
+                            meta: false, // to prevent adding draft-06 meta-schema
+                            allErrors: true // check all rules collecting all errors
+                        });
+
+                    let validate;
+
+                    // override `uri` format to `uri-reference`
+                    validator.addFormat('uri', validator._formats['uri-reference']);
+
+                    validate = validator.compile(META_SCHEMA);
+
+                    expect(validate(generatedSchema)).to.be.true;
+                    done();
                 });
-
-            let validate;
-
-            // override `uri` format to `uri-reference`
-            validator.addFormat('uri', validator._formats['uri-reference']);
-
-            validate = validator.compile(META_SCHEMA);
-
-            expect(validate(generatedSchema)).to.be.true;
-            done();
+            });
         });
     });
 });
